717a6e7ab409db65037c118a0fa4779303cc898d
Jason Hitching <jasonhitching1994@gmail.com>
2020-09-15 12:04:19 +0100

    added: Semantic UI framework

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { Header, Icon } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    values: [],
+  };
+
+  // When component mounts use axios to retrieve data from the project API
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/values").then((response) => {
+      console.log(response);
+      this.setState({
+        // values array within state property = API response data
+        values: response.data,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header as="h2">
+          <Icon name="plug" />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <ul>
+          {this.state.values.map((value: any) => (
+            <li key={value.id}>{value.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
