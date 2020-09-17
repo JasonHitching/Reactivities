c57ceaab784908f9c6811621c2ac7c3542495f02
Jason Hitching <jasonhitching1994@gmail.com>
2020-09-17 09:01:25 +0100

    feat: Activity details Mediatr handler implemented
    - Details handler enables for a Activity ID to be queried
    - Full details of activity corresponding to ID param returned

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
@@ -23,5 +24,12 @@ namespace API.Controllers
         {
             return await _mediator.Send(new List.Query());
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> Details(Guid id)
+        {
+            return await _mediator.Send(new Details.Query { Id = id });
+        }
+
     }
 }
