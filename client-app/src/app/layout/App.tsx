cbf33a2e654cc4603eb1ceff20940ecc45e6d442
Jason Hitching <jasonhitching1994@gmail.com>
2020-10-02 07:53:43 +0100

    deleted: redundant react files

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import axios from "axios";
 import { Header, Icon, List } from "semantic-ui-react";
+import { IActivity } from "../models/activity";
 
-class App extends Component {
-  state = {
-    values: [],
+interface IState {
+  activities: IActivity[]
+}
+
+class App extends Component<{}, IState> {
+  state: IState = {
+    activities: [],
   };
 
   // When component mounts use axios to retrieve data from the project API
   componentDidMount() {
-    axios.get("http://localhost:5000/api/values").then((response) => {
+    axios.get("http://localhost:5000/api/activities").then((response) => {
       this.setState({
         // values array within state property = API response data
-        values: response.data,
+        activities: response.data,
       });
     });
   }
@@ -27,8 +30,8 @@ class App extends Component {
           <Header.Content>Reactivities</Header.Content>
         </Header>
         <List>
-          {this.state.values.map((value: any) => (
-            <List.Item key={value.id}>{value.name}</List.Item>
+          {this.state.activities.map((activity: any) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
           ))}
         </List>
       </div>
