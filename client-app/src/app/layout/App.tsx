356f8fff25b760afd799d2c96cfbe107284cb036
Jason Hitching <jasonhitching1994@gmail.com>
2020-10-02 14:59:39 +0100

    modified: class component replaced with a hook

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,42 +1,31 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Header, Icon, List } from "semantic-ui-react";
 import { IActivity } from "../models/activity";
 
-interface IState {
-  activities: IActivity[]
-}
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
 
-class App extends Component<{}, IState> {
-  state: IState = {
-    activities: [],
-  };
-
-  // When component mounts use axios to retrieve data from the project API
-  componentDidMount() {
-    axios.get("http://localhost:5000/api/activities").then((response) => {
-      this.setState({
-        // values array within state property = API response data
-        activities: response.data,
-      });
+  useEffect(() => {
+    //Axios get request to return 
+    axios.get<IActivity[]>("http://localhost:5000/api/activities").then((response) => {
+      setActivities(response.data);
     });
-  }
+  });
 
-  render() {
-    return (
-      <div>
-        <Header as="h2">
-          <Icon name="plug" />
-          <Header.Content>Reactivities</Header.Content>
-        </Header>
-        <List>
-          {this.state.activities.map((activity: any) => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header as="h2">
+        <Icon name="plug" />
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+        {activities.map((activity) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
 }
 
 export default App;
