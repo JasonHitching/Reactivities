c9654fc55b1150ed996b45dfd4ee1d2a315e5a07
Jason Hitching <jasonhitching1994@gmail.com>
2020-10-05 12:29:02 +0100

    added: navbar and styling
    Added a semantic UI navbar and various space styling

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Header, Icon, List } from "semantic-ui-react";
+import { Container, List } from "semantic-ui-react";
 import { IActivity } from "../models/activity";
-
+import { NavBar } from "../../features/NavBar";
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
-
   useEffect(() => {
     //Axios get request to return a list of activities from the API
     axios
@@ -16,13 +15,16 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      <List>
-        {activities.map((activity) => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
-    </div>
+    <>
+      <NavBar />
+      <Container className="act-list">
+        <List>
+          {activities.map((activity) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+      </Container>
+    </>
   );
 };
 
